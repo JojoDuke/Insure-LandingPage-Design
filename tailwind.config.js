51da5bc8993f526473f5d65aede385d0bedd879f
Duke Opoku-Amankwa <jojoamankwa@gmail.com>
2021-01-14 14:20:22 +0000

    Working on it

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        karla: ["Karla"],
+        dmSerif: ["DM Serif Display"]
+      },
+      colors: {
+        violet: {
+          dark: 'hsl(256, 26%, 20%)',
+          veryDark: 'hsl(270, 9%, 17%)',
+          grayDark: 'hsl(273, 4%, 51%)'
+        },
+        grayBlue: 'hsl(216, 30%, 68%)',
+        grayLight: 'hsl(0, 0%, 98%)'
+      }
+    },
   },
   variants: {
     extend: {},
